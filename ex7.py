3b065fba70d026d4c12c7e872fccfff102105d50
corsapreto2009 <diogocalveche27@gmail.com>
2025-09-02 17:04:29 -0300

    finalmente terminei...

---
+++ ex7.py
@@ -0,0 +1,6 @@
+mediafinal = int(input("qual foi sua média final?"))
+
+if mediafinal >= 6:
+    print("parabéns! você foi aprovado!")
+else:
+    print("você foi reprovado... mais sorte da próxima!")
