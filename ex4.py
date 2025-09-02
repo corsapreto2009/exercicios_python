85b773c651f3e308b30388874c62d95afe5f9cd7
corsapreto2009 <diogocalveche27@gmail.com>
2025-09-02 15:05:21 -0300

    continuação de um dia antes da entrega

---
+++ ex4.py
@@ -0,0 +1,8 @@
+a = int(input("me diga a altura da sua parede em metros (não é necessário colocar m ao lado. somente o número.)"))
+l = int(input("agora me dig a largura de sua parede."))
+
+area = a * l
+tinta = area / 2
+
+print("a área da sua parede é" + area, "cm²")
+print("e o tanto de tinta que você terá de usar vai ser" + tinta, "litros")
